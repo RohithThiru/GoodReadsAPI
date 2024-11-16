45bcbb2aaae055fcb3e821b2d8ac54a464921e6a
Rohith T <142724079+RohithThiru@users.noreply.github.com>
2024-11-16 22:51:37 +0530

    H2 Database connectivity is added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into book(name, imageUrl) values ('Harry Potter and the Philosopher"s Stone','harry potter.jpg');
+insert into book(name, imageUrl) values ('Harry Potter and the Philosopher"s Stone','harry potter.jpg');
+insert into book(name, imageUrl) values ('Harry Potter and the Philosopher"s Stone','harry potter.jpg');
+insert into book(name, imageUrl) values ('Harry Potter and the Philosopher"s Stone','harry potter.jpg');
+insert into book(name, imageUrl) values ('Rohith Potter and the Philosopher"s Stone','harry potter.jpg');
